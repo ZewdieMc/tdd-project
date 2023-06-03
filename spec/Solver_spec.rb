7cd79284ef06c75837af0f4d0d9150d469f182dd
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-03 08:03:06 +0300

    Add test for factorial of negative numbers

--- spec/Solver_spec.rb
+++ spec/Solver_spec.rb
@@ -15,4 +15,8 @@ describe Solver do
   it "should solve factorial of 0" do
     expect(@solver.factorial(0)).to eq(1)
   end
+
+  it "Should raise an error if the argument is negative" do
+    expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
+  end
 end
