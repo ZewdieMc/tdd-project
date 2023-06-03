6cd50c87b5de1be2f717670d75b33e8486605ad7
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-03 07:33:41 +0300

    Create empty solver class

--- spec/Solver_spec.rb
+++ spec/Solver_spec.rb
@@ -1,4 +1,10 @@
 require './solver'
 
 describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+  it "should be an instance of Solver" do
+    expect(@solver).to be_an_instance_of Solver
+  end
 end
