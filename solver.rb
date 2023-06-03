adfb266a90a704d61e2c4937a6d816f2d534da06
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-03 08:41:49 +0300

    Add logic for fizbuzz method

--- solver.rb
+++ solver.rb
@@ -9,4 +9,12 @@ class Solver
   def reverse(str)
     str.reverse
   end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
 end
