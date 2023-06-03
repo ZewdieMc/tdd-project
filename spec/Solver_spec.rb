72f4e976841f05d2335c8c9bb4e5a4f783ae3e62
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-03 07:37:13 +0300

    Add test for factorial(5)=120

--- spec/Solver_spec.rb
+++ spec/Solver_spec.rb
@@ -7,4 +7,8 @@ describe Solver do
   it "should be an instance of Solver" do
     expect(@solver).to be_an_instance_of Solver
   end
+
+  it "should have a solve method" do
+    expect(@solver.factorial(5)).to eq(120)
+  end
 end
