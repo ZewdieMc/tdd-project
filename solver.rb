73c203184f902224e2db32c989baae9106a5dfa6
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-03 08:32:41 +0300

    Add test for reverse method

--- solver.rb
+++ solver.rb
@@ -1,7 +1,8 @@
 class Solver
-  def factorial(n)
-    raise ArgumentError if n < 0
-    return 1 if n == 0
-    n * factorial(n-1)
+  def factorial(num)
+    raise ArgumentError if num.negative?
+    return 1 if num.zero?
+
+    num * factorial(num - 1)
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,19 +4,23 @@ describe Solver do
   before :each do
     @solver = Solver.new
   end
-  it "should be an instance of Solver" do
+  it 'should be an instance of Solver' do
     expect(@solver).to be_an_instance_of Solver
   end
 
-  it "should solve factorial of 5" do
+  it 'should solve factorial of 5' do
     expect(@solver.factorial(5)).to eq(120)
   end
 
-  it "should solve factorial of 0" do
+  it 'should solve factorial of 0' do
     expect(@solver.factorial(0)).to eq(1)
   end
 
-  it "Should raise an error if the argument is negative" do
+  it 'Should raise an error if the argument is negative' do
     expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
+  end
+
+  it 'Should reverse a string' do
+    expect(@solver.reverse('hello')).to eq('olleh')
   end
 end
